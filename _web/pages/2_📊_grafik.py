4f196cc56ab25eecc0869151fe96ce406f289472
Yasin Ünal <yasin.unal@bil.omu.edu.tr>
2023-03-15 21:49:44 +0300

    home

--- _web/pages/2_📊_grafik.py
+++ _web/pages/2_📊_grafik.py
@@ -8,16 +8,35 @@ import yfinance as yf
 st.set_page_config(layout="wide")
 
 
-option = st.selectbox()
 
-kod = {}
+kod = {"BIST30":"XU030.IS", "BIST100":"XU100.IS", "Türk Hava Yollari":"THYAO.IS" }
 # XU100.IS 
 # THYAO.IS
+
+option = st.selectbox(
+    'Seçiminiz',
+    ('BIST30', 'BIST100','Türk Hava Yollari'))
+
+col1 , col2 = st.columns(2)
+col1.write('You selected:', option)
+col2.write("borsa : ",kod[option])
+
+
+
+st.write('You selected:', option)
+st.write(type(option))
+
+st.write("borsa : ",kod[option])
+st.write("tip : ", type(kod[option]))
+
+
 today = date.today().strftime('%Y-%m-%d')
 # Burada yahoo finans kullanarak BİST30 -> XU030.IS verilerini inceleyebileceğiz. Herhangi ekstra dosya olmadan verileri alacağız.
 df = yf.download("XU030.IS", 
                 start="2020-09-22", 
                 end=today)
                  
+
+
 
 st.write(df)
